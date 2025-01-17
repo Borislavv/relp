1de0841f5f14498204c645584dc7637237b1762a
Boris <glazunov2142@icloud.com>
2025-01-17 09:18:59 +0300

    Create executor.rs

---
+++ src/infrastructure/service/executor/executor.rs
@@ -0,0 +1,20 @@
+use reqwest::Error;
+use crate::infrastructure::service::command::command::Executable;
+use crate::infrastructure::service::executor::responder::Responder;
+
+pub trait Executor: Send + Sync {
+    fn exec(&self, cmd: Box<dyn Executable>) -> Result<(), Error>;
+}
+pub struct CommandExecutor {
+    responder: Box<dyn Responder>,
+}
+impl CommandExecutor {
+    pub fn new(responder: Box<dyn Responder>) -> Self {
+        Self{ responder }
+    }
+}
+impl Executor for CommandExecutor {
+    fn exec(&self, cmd: Box<dyn Executable>) -> Result<(), Error>  {
+        self.responder.respond(cmd.exec())
+    }
+}

75c96aec398c470f85558abb60c08e580af0e7de
Boris <glazunov2142@icloud.com>
2025-01-17 09:20:44 +0300

    Create model.rs

---
+++ src/infrastructure/integration/telegram/model.rs
@@ -0,0 +1,50 @@
+use serde::{Deserialize, Serialize};
+
+// List of updates.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GetUpdatesResponse {
+    pub ok: bool,
+    pub result: Vec<Update>,
+}
+// SendMessageResponse struct present a telegram response on sendMessage method.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SendMessageResponse {
+    pub ok: bool,
+    pub result: Message,
+}
+
+// Update is a single message structure. Telegram sends a list of Update structs.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Update {
+    pub update_id: i64,
+    pub message: Option<Message>,
+    pub edited_message: Option<Message>,
+}
+// Message details.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Message {
+    pub message_id: i64,
+    pub from: User,
+    pub chat: Chat,
+    pub date: i64,
+    pub text: String,
+}
+// User details.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct User {
+    pub id: i64,
+    pub is_bot: bool,
+    pub first_name: String,
+    pub username: String,
+    pub language_code: Option<String>,
+    pub is_premium: Option<bool>,
+}
+// Chat details.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Chat {
+    pub id: i64,
+    pub first_name: String,
+    pub username: String,
+    pub r#type: String,
+}
+

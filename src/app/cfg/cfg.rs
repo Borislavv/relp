9d12fde6861872eacc9625ec691d42912970279d
Boris <glazunov2142@icloud.com>
2025-01-17 19:31:27 +0300

    Update cfg.rs

--- src/app/cfg/cfg.rs
+++ src/app/cfg/cfg.rs
@@ -11,8 +11,8 @@ pub struct Cfg {
 impl Cfg {
     pub fn new() -> Result<Self, env::VarError> {
         let s = Self {
-            token: env::var("TG_TOKEN").unwrap_or("8124548645:AAE-3yG8AZTmZRvB5Y0K8qhuVopLhsSFYE0".to_string()),
-            chat_id: env::var("TG_CHAT_ID").unwrap_or("1063099947".to_string()).parse::<u64>().unwrap(),
+            token: env::var("TG_TOKEN").unwrap_or("secret_token".to_string()),
+            chat_id: env::var("TG_CHAT_ID").unwrap_or("your_chat_id".to_string()).parse::<u64>().unwrap(),
             poll_frequency: Duration::from_secs(env::var("TG_POLL_FREQUENCY_SEC").unwrap_or("5".to_string()).parse().unwrap()),
         };
 

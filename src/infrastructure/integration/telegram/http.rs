bc46b03853a9310c656a03ce3729039e1f6d1bb1
Boris <glazunov2142@icloud.com>
2025-01-17 09:17:58 +0300

    Update http.rs

--- src/infrastructure/integration/telegram/http.rs
+++ src/infrastructure/integration/telegram/http.rs
@@ -1,4 +1,3 @@
-use std::panic::RefUnwindSafe;
 use std::time::Duration;
 use reqwest::blocking::{Client as ReqwestClient, Response};
 use reqwest::Error;
@@ -7,7 +6,7 @@ const TELEGRAM_API_URL: &str = "https://api.telegram.org";
 const TELEGRAM_API_SEND_MESSAGE_METHOD: &str = "sendMessage";
 const TELEGRAM_API_FETCH_MESSAGES_METHOD: &str = "getUpdates";
 
-pub trait HttpClient: Send + Sync + RefUnwindSafe {
+pub trait HttpClient: Send + Sync {
     fn send_message(&self, chat_id: u64, msg: &str) -> Result<Response, Error>;
     fn get_updates(&self, offset: i64) -> Result<Response, Error>;
 }

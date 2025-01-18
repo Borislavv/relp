3603c8a1185f3875cefb70689f67cbcec1d68d7e
Boris <glazunov2142@icloud.com>
2025-01-19 01:17:57 +0300

    Update consumer.rs

--- src/infrastructure/service/message/consumer.rs
+++ src/infrastructure/service/message/consumer.rs
@@ -1,28 +1,35 @@
+use std::sync::Arc;
 use log::{error, info};
 use integration::telegram;
 use std::sync::mpsc::Receiver;
+use crate::app::model::state::State;
 use crate::infrastructure::integration;
 use crate::domain::factory::command::Factoryer;
 use crate::domain::service::executor::executor::Executor;
 
-pub trait Consumer: Send {
+pub trait Consumer: Send + Sync {
     fn consume(&self, ch: Receiver<telegram::model::Message>);
 }
 
 pub struct MessageConsumer {
     executor: Box<dyn Executor>,
     factory: Box<dyn Factoryer>,
+    state: Arc<Box<dyn State>>,
 }
 
 impl MessageConsumer {
-    pub fn new(executor: Box<dyn Executor>, factory: Box<dyn Factoryer>) -> MessageConsumer {
-        MessageConsumer { executor, factory }
+    pub fn new(executor: Box<dyn Executor>, factory: Box<dyn Factoryer>, state: Arc<Box<dyn State>>,) -> MessageConsumer {
+        MessageConsumer { executor, factory, state }
     }
 }
 
 impl Consumer for MessageConsumer {
     fn consume(&self, msg_ch: Receiver<telegram::model::Message>) {
         for msg in msg_ch {
+            if self.state.is_closed() {
+                return;
+            }
+
             match self.executor.exec(self.factory.make(msg.clone())) {
                 Ok(_) => info!("Command: {} successfully executed.", msg.text),
                 Err(e) => error!("Error: {} occurred while execution command: {}.", e, msg.text),

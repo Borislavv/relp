43185034af9a218642b5568166c45891753bea37
Boris <glazunov2142@icloud.com>
2025-01-19 01:18:01 +0300

    Update service.rs

--- src/infrastructure/integration/telegram/service.rs
+++ src/infrastructure/integration/telegram/service.rs
@@ -4,21 +4,21 @@ use crate::infrastructure::integration;
 use integration::telegram::http::HttpClient;
 use integration::telegram::model::{GetUpdatesResponse, SendMessageResponse};
 
-pub trait ServiceTrait: Send + Sync {
+pub trait TelegramServiceTrait: Send + Sync {
     fn get_updates(&self, offset: i64) -> Result<GetUpdatesResponse, Box<dyn Error>>;
     fn send_message(&self, chat_id: u64, message: &str) -> Result<SendMessageResponse, Box<dyn Error>>;
 }
 
-pub struct Service {
+pub struct TelegramService {
     http_client: Box<dyn HttpClient>
 }
-impl Service {
+impl TelegramService {
     pub fn new(http_client: Box<dyn HttpClient>) -> Self {
         Self { http_client }
     }
 }
 
-impl ServiceTrait for Service {
+impl TelegramServiceTrait for TelegramService {
     fn get_updates(&self, offset: i64) -> Result<GetUpdatesResponse, Box<dyn Error>> {
         let data = self.http_client.get_updates(offset)?.text()?;
         match serde_json::from_str(&data) {

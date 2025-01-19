62a53a4d9371f79d82b32dc7bff1d3a773945322
Boris <glazunov2142@icloud.com>
2025-01-19 10:09:48 +0300

    Update cfg.rs

--- src/app/cfg/cfg.rs
+++ src/app/cfg/cfg.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::time::Duration;
-use log::info;
 
 #[derive(Clone)]
 pub struct Cfg {
@@ -11,15 +10,15 @@ pub struct Cfg {
 impl Cfg {
     pub fn new() -> Result<Self, env::VarError> {
         let s = Self {
-            token: env::var("TG_TOKEN").unwrap_or("secret_token".to_string()),
+            token: env::var("TG_TOKEN").unwrap_or("telegram_token".to_string()),
             chat_id: env::var("TG_CHAT_ID").unwrap_or("123456789".to_string()).parse::<u64>()
                 .unwrap(),
             poll_frequency: Duration::from_secs(env::var("TG_POLL_FREQUENCY_SEC").unwrap_or("5".to_string()).parse().unwrap()),
         };
 
-        info!("Using environment variable TG_TOKEN={}", s.token);
-        info!("Using environment variable TCH_CHAT_ID={}", s.chat_id);
-        info!("Using environment variable TG_TOKEN={}", s.token);
+        println!("Using environment variable TG_TOKEN={}", s.token);
+        println!("Using environment variable TCH_CHAT_ID={}", s.chat_id);
+        println!("Using environment variable TG_TOKEN={}", s.token);
 
         Ok(s)
     }

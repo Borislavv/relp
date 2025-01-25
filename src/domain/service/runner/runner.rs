ac6abd837cb02a92ffcfe1b11181f8e4c66589f2
Boris <glazunov2142@icloud.com>
2025-01-25 11:01:19 +0300

    Update runner.rs

--- src/domain/service/runner/runner.rs
+++ src/domain/service/runner/runner.rs
@@ -1,14 +1,18 @@
+use crate::app::cfg::cfg::Cfg;
+use crate::domain::service::command;
+use crate::domain::service::event::r#loop::EventLoop;
+use crate::infrastructure::service::message;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
-use std::sync::{mpsc, Arc, Mutex};
-use crate::domain::service::command;
-use crate::infrastructure::service::message;
 
 pub trait Runner {
     fn run(&self) -> ();
 }
 
 pub struct AppRunner {
+    cfg: Cfg,
+    event_loop: Arc<Box<dyn EventLoop>>,
     worker: Arc<Box<dyn command::worker::Worker>>,
     provider: Arc<Mutex<Box<dyn message::provider::Provider>>>,
     consumer: Arc<Mutex<Box<dyn message::consumer::Consumer>>>,
@@ -16,40 +20,45 @@ pub struct AppRunner {
 
 impl AppRunner {
     pub fn new(
+        cfg: Cfg,
+        event_loop: Arc<Box<dyn EventLoop>>,
         worker: Arc<Box<dyn command::worker::Worker>>,
         provider: Arc<Mutex<Box<dyn message::provider::Provider>>>,
         consumer: Arc<Mutex<Box<dyn message::consumer::Consumer>>>,
     ) -> AppRunner {
-        AppRunner { worker, provider, consumer }
+        AppRunner { cfg, event_loop, worker, provider, consumer }
     }
 }
 
 impl Runner for AppRunner {
     fn run(&self) -> () {
-        let (s, r) = mpsc::sync_channel(100);
+        let (sender, receiver) = mpsc::sync_channel(self.cfg.event_loop_channel_capacity);
         let worker = self.worker.clone();
         let provider = self.provider.clone();
         let consumer = self.consumer.clone();
+        let event_loop = self.event_loop.clone();
 
         let mut threads: Vec<JoinHandle<()>> = Vec::new();
 
-        threads.push(
-            thread::spawn(move || {
-                worker.serve();
-            })
-        );
+        threads.push(thread::spawn(move || {
+            // starts a daemon for serve deferred events.
+            worker.serve();
+        }));
 
-        threads.push(
-            thread::spawn(move || {
-                provider.lock().unwrap().provide(s);
-            })
-        );
+        threads.push(thread::spawn(move || {
+            // starts a long poller which asks telegram message updates and provides them into output channel.
+            provider.lock().unwrap().provide(sender);
+        }));
 
-        threads.push(
-            thread::spawn(move || {
-                consumer.lock().unwrap().consume(r);
-            })
-        );
+        threads.push(thread::spawn(move || {
+            // starts a consumer of channel which uses provider as the output.
+            consumer.lock().unwrap().consume(receiver);
+        }));
+
+        threads.push(thread::spawn(move || {
+            // starts an event loop which will serve different events in app
+            event_loop.serve();
+        }));
 
         for descriptor in threads {
             descriptor.join().unwrap();

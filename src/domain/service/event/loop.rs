0abf72d5f23e687f98a9f09d3674eb891e0c1090
Boris <glazunov2142@icloud.com>
2025-01-25 11:01:26 +0300

    Update loop.rs

--- src/domain/service/event/loop.rs
+++ src/domain/service/event/loop.rs
@@ -1,29 +1,34 @@
 use crate::app;
-use std::sync::Mutex;
-use crate::domain::service::event::model::Event;
+use crate::domain::service::event::model::{Event, ExecutableEvent};
+use crate::domain::service::executor::executor::Executor;
 use crate::infrastructure::broadcasting::mpsc::channel::Channel;
+use std::sync::{Arc, Mutex};
 
-pub trait EventLoop {
+pub trait EventLoop: Send + Sync {
     fn serve(&self);
-    fn add_event(&self, event: Box<dyn Event>);
+    fn add_event(&self, event: Arc<Box<dyn Event>>);
 }
 
 pub struct CommandEventLoop {
-    heap: Mutex<Vec<Box<dyn Event>>>,
-    state: Box<dyn app::model::state::State>,
-    channel: Box<dyn Channel<Box<dyn Event>>>,
+    state: Arc<Box<dyn app::model::state::State>>,
+    events: Mutex<Vec<Arc<Box<dyn ExecutableEvent>>>>,
+    channel: Box<dyn Channel<Arc<Box<dyn ExecutableEvent>>>>,
+    executor: Arc<Box<dyn Executor>>,
 }
 
 impl CommandEventLoop {
-    pub fn new(state: Box<dyn app::model::state::State>, channel: Box<dyn Channel<Box<dyn Event>>>) -> Self {
-        Self { heap: Mutex::new(vec![]), state, channel }
+    pub fn new(
+        state: Arc<Box<dyn app::model::state::State>>,
+        channel: Box<dyn Channel<Arc<Box<dyn Event>>>>,
+        executor: Arc<Box<dyn Executor>>
+    ) -> Self {
+        Self { state, events: Mutex::new(vec![]), channel, executor }
     }
 }
 
 impl EventLoop for CommandEventLoop {
-    // serve - infinitely iterating over events and checks whether one or more events
-    // will be ready for send to them Receivers.
-    //
+    // Method serve is infinitely iterating over events and checks whether one or more events
+    // will be ready for send to them Receivers or execute at place (depends on if an event has a sender into or not).
     // Technical details: have a timeout between each new iteration in 1 second due to decrease CPU consumption.
     fn serve(&self) {
         loop {
@@ -31,30 +36,23 @@ impl EventLoop for CommandEventLoop {
             if self.state.is_closed() { return; }
 
             // (vec![]).drain(0..) -> removes all elements from vector, if an event is not ready, you need put it back
-            for event in self.heap.lock().unwrap().drain(0..) {
-                let key = event.key();
-
+            for event in self.events.lock().unwrap().drain(0..) {
                 if event.is_ready() {
-                    if let Err(e) = self.channel.sender(key.clone()) {
-                        eprintln!("Sender was not found into the map, the Event was skipped: {}.", e);
-
-                        // return the event which hasn't an own sender back to the heap
-                        self.heap.lock().unwrap().push(event);
-
-                        // create missed Sender and Receiver of current Event
-                        self.channel.add(key);
-
-                        // go to new iteration
-                        continue;
-                    }
-
-                    if let Some(sender) = self.channel.sender(key.clone()) {
-                        // send the ready event to the receiver (already removed from the events vector)
-                        sender.send(event).unwrap();
-                    }
+                    match event.sender() {
+                        Some(readyEvent) => {
+                            // unwrap is safe if you do not have a failures in another thread which consume from receiver
+                            readyEvent.send(event).unwrap()
+                        },
+                        None => {
+                            match self.executor.exec(event.clone()) {
+                                Ok(_) => println!("Command: {} successfully executed.", event.name()),
+                                Err(e) => println!("Error: {} occurred while execution command: {}.", e, event.name()),
+                            };
+                        },
+                    };
                 } else {
                     // return the event which is not ready back to the vector
-                    self.heap.lock().unwrap().push(event);
+                    self.events.lock().unwrap().push(event);
                 }
             }
             // limitation of CPU consumption (once in a second)
@@ -64,13 +62,8 @@ impl EventLoop for CommandEventLoop {
 
     // add_event - pushes new event into the loop event heap and creates a new one pair
     // of Sender and Receiver for event.key() if necessary of course.
-    fn add_event(&self, event: Box<dyn Event>) {
-        // if (sender, receiver) are not exist for event.key()
-        if let Err(_) = self.channel.sender(event.key()) {
-            // then add new (sender, receiver) by key
-            self.channel.add(event.key());
-        }
+    fn add_event(&self, event: Arc<Box<dyn ExecutableEvent>>) {
         // handle lock and push new event into the loop
-        self.heap.lock().unwrap().push(event);
+        self.events.lock().unwrap().push(event);
     }
 }

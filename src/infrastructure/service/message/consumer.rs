06241256dcf105492f72efa6a90cba6f103694d5
Boris <glazunov2142@icloud.com>
2025-01-25 11:00:53 +0300

    Update consumer.rs

--- src/infrastructure/service/message/consumer.rs
+++ src/infrastructure/service/message/consumer.rs
@@ -1,10 +1,12 @@
-use std::sync::Arc;
+use crate::app::model::state::State;
+use crate::domain::factory::command::Factoryer;
+use crate::domain::service::event::model::ExecutableEvent;
+use crate::domain::service::event::r#loop::EventLoop;
+use crate::domain::service::executor::executor::Executor;
+use crate::infrastructure::integration;
 use integration::telegram;
 use std::sync::mpsc::Receiver;
-use crate::app::model::state::State;
-use crate::infrastructure::integration;
-use crate::domain::factory::command::Factoryer;
-use crate::domain::service::executor::executor::Executor;
+use std::sync::Arc;
 
 pub trait Consumer: Send + Sync {
     fn consume(&self, ch: Receiver<telegram::model::Message>);
@@ -14,11 +16,17 @@ pub struct MessageConsumer {
     executor: Box<dyn Executor>,
     factory: Box<dyn Factoryer>,
     state: Arc<Box<dyn State>>,
+    event_loop: Arc<Box<dyn EventLoop>>,
 }
 
 impl MessageConsumer {
-    pub fn new(executor: Box<dyn Executor>, factory: Box<dyn Factoryer>, state: Arc<Box<dyn State>>) -> MessageConsumer {
-        MessageConsumer { executor, factory, state }
+    pub fn new(
+        executor: Box<dyn Executor>,
+        factory: Box<dyn Factoryer>,
+        state: Arc<Box<dyn State>>,
+        event_loop: Arc<Box<dyn EventLoop>>
+    ) -> MessageConsumer {
+        MessageConsumer { executor, factory, state, event_loop }
     }
 }
 
@@ -29,7 +37,11 @@ impl Consumer for MessageConsumer {
                 return;
             }
 
-            match self.executor.exec(self.factory.make(msg.clone())) {
+            let event: Arc<Box<dyn ExecutableEvent>> = Arc::new(self.factory.make(msg.clone()));
+
+            self.event_loop.clone().add_event(event);
+
+            match self.executor.exec(executable) {
                 Ok(_) => println!("Command: {} successfully executed.", msg.text),
                 Err(e) => println!("Error: {} occurred while execution command: {}.", e, msg.text),
             }

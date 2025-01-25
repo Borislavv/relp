c8d7bd5d085f6568e98b740e59af3ba35ba31ebd
Boris <glazunov2142@icloud.com>
2025-01-25 11:01:08 +0300

    Update channel.rs

--- src/infrastructure/broadcasting/mpsc/channel.rs
+++ src/infrastructure/broadcasting/mpsc/channel.rs
@@ -1,57 +1,57 @@
+use crate::infrastructure::broadcasting::mpsc::error::{AlreadyExistsError, NoEntryWasFoundError, SendOnClosedChannelError};
 use std::collections::HashMap;
-use std::sync::{mpsc, Arc, Mutex};
 use std::sync::mpsc::{Receiver, Sender};
-use crate::infrastructure::broadcasting::mpsc::error::{NoEntryWasFoundError, SendOnClosedChannelError};
+use std::sync::{mpsc, Arc, Mutex};
 
-pub trait Channel<T>: Send + Sync {
-    // adds a new one pair of sender and receiver by key
-    fn add(&self, key: String);
+pub trait Channel<T: Send + Sync + Clone>: Send + Sync {
+    // adds a new one receiver by key
+    fn add(&self, key: String) -> Result<Receiver<T>, AlreadyExistsError>;
     // sends the data to one receiver by key or to all receivers in case when key is None
     fn send(&self, key: Option<String>, data: T) -> Result<(), SendOnClosedChannelError>;
-    // removes the target pair of sender and receiver by key
+    // removes the target receiver by key
     fn remove(&self, key: String);
     // returns a target sender by key
-    fn sender(&self, key: String) -> Result<Sender<T>, NoEntryWasFoundError>;
-    // returns a target receiver by key
-    fn receiver(&self, key: String) -> Result<Receiver<T>, NoEntryWasFoundError>;
+    fn sender(&self, key: Option<String>) -> Result<Vec<Arc<Sender<T>>>, NoEntryWasFoundError>;
 }
 
 pub struct Chan<T> {
-    map: Arc<Mutex<HashMap<String, (Sender<T>, Receiver<T>)>>>,
+    map: Arc<Mutex<HashMap<String, Arc<Sender<T>>>>>,
 }
 
 impl <T> Chan<T> {
     pub fn new() -> Chan<T> {
-        let map: HashMap<String, (Sender<T>, Receiver<T>)> = HashMap::new();
+        let map: HashMap<String, Arc<Sender<T>>> = HashMap::new();
         Self { map: Arc::new(Mutex::new(map)) }
     }
 }
 
-impl <T> Channel<T> for Chan<T> {
-    fn add(&self, key: String) {
-        let (s, r) = mpsc::channel::<T>();
+impl <T: Send + Sync + Clone> Channel<T> for Chan<T> {
+    fn add(&self, key: String) -> Result<Receiver<T>, AlreadyExistsError> {
+        let (sender, receiver) = mpsc::channel::<T>();
         // unwrap here is safe due to only already exists concurrency problems can have affect it
         // for example, if other thread was locked mutex and had failed without unlocking it (deadlock).
         let mut map = self.map.lock().unwrap();
 
         if map.contains_key(&key) {
-            return;
+            return Err(AlreadyExistsError::new("map key already exists".to_string()));
         }
 
-        map.insert(key, (s, r));
+        map.insert(key, Arc::new(sender));
+
+        Ok(receiver)
     }
 
-    fn send(&self, key: Option<String>, data: Arc<T>) -> Result<(), SendOnClosedChannelError> {
+    fn send(&self, key: Option<String>, data: T) -> Result<(), SendOnClosedChannelError> {
         if let Some(key) = key {
             let mut map = self.map.lock().unwrap();
-            let sender = map.get(&key).unwrap().0.clone();
-            if let Err(e) = sender.send(Some(data.clone())) {
+            let sender = map.get(&key).unwrap();
+            if let Err(e) = sender.send(data.clone()) {
                 return Err(SendOnClosedChannelError::new(e.to_string()));
             }
         } else {
             for included_map in self.map.lock().unwrap().iter() {
-                let sender = included_map.1.0.clone();
-                if let Err(e) = sender.send(Some(data.clone())) {
+                let sender = included_map.1.clone();
+                if let Err(e) = sender.send(data.clone()) {
                     return Err(SendOnClosedChannelError::new(e.to_string()));
                 }
             }
@@ -63,23 +63,20 @@ impl <T> Channel<T> for Chan<T> {
         self.map.lock().unwrap().remove(&key);
     }
 
-    fn sender(&self, key: String) -> Result<Sender<T>, NoEntryWasFoundError> {
+    fn sender(&self, key: Option<String>) -> Result<Vec<Arc<Sender<T>>>, NoEntryWasFoundError> {
         let mut map = self.map.lock().unwrap();
 
-        if !map.contains_key(&key) {
-            return Err(NoEntryWasFoundError::new());
+        if let Some(key) = key {
+            if !map.contains_key(&key) {
+                return Err(NoEntryWasFoundError::new());
+            }
+            Ok(vec![map.get(&key).unwrap().clone()])
+        } else {
+            let mut senders = Vec::with_capacity(map.len());
+            for (_, map) in self.map.lock().unwrap().iter() {
+                senders.push(map.clone());
+            }
+            Ok(senders)
         }
-
-        Ok(map.get(&key).unwrap().0.clone())
-    }
-
-    fn receiver(&self, key: String) -> Result<Receiver<T>, NoEntryWasFoundError> {
-        let mut map = self.map.lock().unwrap();
-
-        if !map.contains_key(&key) {
-            return Err(NoEntryWasFoundError::new());
-        }
-
-        Ok(map.get(&key).unwrap().1.clone())
     }
 }

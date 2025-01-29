3848fd2dcaeb276620137f62ceab34ed941dab75
Boris <glazunov2142@icloud.com>
2025-01-29 23:50:27 +0300

    Update message.rs

--- src/domain/error/message.rs
+++ src/domain/error/message.rs
@@ -16,4 +16,28 @@ impl fmt::Display for UnknownMessageTypeError {
     }
 }
 
-impl Error for UnknownMessageTypeError {}
+impl Error for OffsetFetchError {}
+
+#[derive(Debug)]
+pub struct OffsetFetchError {
+    parent: String
+}
+
+impl OffsetFetchError {
+    pub fn new(parent: Option<Box<dyn Error>>) -> OffsetFetchError {
+        OffsetFetchError {
+            parent: match parent {
+                Some(parent) => parent.to_string(),
+                None => String::from("unknown reason"),
+            }
+        }
+    }
+}
+
+impl fmt::Display for OffsetFetchError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Failed to fetch offset: {}.", self.parent.to_string())
+    }
+}
+
+impl Error for OffsetFetchError {}

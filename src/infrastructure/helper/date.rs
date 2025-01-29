f9576fec6bc7fa961b89fbe0555c0929c3096ea3
Boris <glazunov2142@icloud.com>
2025-01-29 13:47:00 +0300

    Update date.rs

--- src/infrastructure/helper/date.rs
+++ src/infrastructure/helper/date.rs
@@ -2,20 +2,24 @@ use crate::domain::error::date::DateTimeParseError;
 use regex::Regex;
 
 // Pattern: YYYY-MM-DD H:i, example: 2025-01-16.
-pub fn parse_yyyy_mm_dd_hm_from_str(s: &str)
-    -> Result<chrono::NaiveDateTime, DateTimeParseError>
-{
+pub fn parse_yyyy_mm_dd_hm_from_str(s: &str) -> Result<chrono::NaiveDateTime, DateTimeParseError> {
     let regex_ymd_t_hm = Regex::new(r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2})").unwrap();
     let regex_ymd_hm = Regex::new(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2})").unwrap();
 
     for capture in regex_ymd_t_hm.captures_iter(s.trim()) {
-        if let Ok(date) = chrono::NaiveDateTime::parse_from_str(capture.get(1).unwrap().as_str(), "%Y-%m-%dT%H:%M") {
+        if let Ok(date) = chrono::NaiveDateTime::parse_from_str(
+            capture.get(1).unwrap().as_str(),
+            "%Y-%m-%dT%H:%M",
+        ) {
             return Ok(date);
         }
     }
 
     for capture in regex_ymd_hm.captures_iter(s.trim()) {
-        if let Ok(date) = chrono::NaiveDateTime::parse_from_str(capture.get(1).unwrap().as_str(), "%Y-%m-%d %H:%M") {
+        if let Ok(date) = chrono::NaiveDateTime::parse_from_str(
+            capture.get(1).unwrap().as_str(),
+            "%Y-%m-%d %H:%M",
+        ) {
             return Ok(date);
         }
     }

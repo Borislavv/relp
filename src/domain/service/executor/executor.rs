38559844d8b8b261e2ee668b9bbbc01767b81fd7
Boris <glazunov2142@icloud.com>
2025-01-29 13:47:27 +0300

    Update executor.rs

--- src/domain/service/executor/executor.rs
+++ src/domain/service/executor/executor.rs
@@ -1,21 +1,22 @@
 use crate::domain::model::command::Executable;
+use crate::domain::model::event::ExecutableEvent;
 use crate::infrastructure::service::executor::responder::Responder;
 use std::error::Error;
 use std::sync::Arc;
 
 pub trait Executor: Send + Sync {
-    fn exec(&self, cmd: Arc<Box<dyn Executable>>) -> Result<(), Box<dyn Error>>;
+    fn exec(&self, cmd: Arc<Box<dyn ExecutableEvent>>) -> Result<(), Box<dyn Error>>;
 }
 pub struct CommandExecutor {
     responder: Box<dyn Responder>,
 }
 impl CommandExecutor {
     pub fn new(responder: Box<dyn Responder>) -> Self {
-        Self{ responder }
+        Self { responder }
     }
 }
 impl Executor for CommandExecutor {
-    fn exec(&self, cmd: Arc<Box<dyn Executable>>) -> Result<(), Box<dyn Error>>  {
+    fn exec(&self, cmd: Arc<Box<dyn ExecutableEvent>>) -> Result<(), Box<dyn Error>> {
         self.responder.respond(cmd.exec())
     }
 }

b5be350fa2daaa9bb035393d0ebd4091541446a0
Boris <glazunov2142@icloud.com>
2025-02-22 09:31:41 +0300

    Create service.rs

---
+++ src/domain/service/wife/message/service.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::sync::{Arc, Mutex, MutexGuard};
+use rand::Rng;
+use rand::seq::IndexedRandom;
+use crate::domain::error::wife::WifeMessagesVecIsEmptyError;
+use crate::domain::model::wife::Message;
+use crate::domain::service::wife::message::parser::MessageParser;
+
+pub trait MessageServiceTrait: Send + Sync {
+    fn get_rand(&self) -> Message;
+}
+
+pub struct MessageService {
+    messages: Mutex<Vec<Message>>,
+}
+
+impl MessageService {
+    pub fn new(message_parser: Arc<Box<dyn MessageParser>>) -> Result<Self, Box<dyn Error>> {
+        let messages = match message_parser.parse() {
+            Ok(message) => message,
+            Err(error) => return Err(error),
+        };
+
+        if messages.is_empty() {
+            return Err(Box::new(WifeMessagesVecIsEmptyError::new()));
+        }
+
+        Ok(Self { messages: Mutex::new(messages) })
+    }
+}
+impl MessageServiceTrait for MessageService {
+    fn get_rand(&self) -> Message {
+        let mut vec: MutexGuard<Vec<Message>> = self.messages.lock().unwrap();
+
+        let mut rng = rand::rng();
+        let random = rng.random::<i32>() as usize;
+
+        let m: Message = vec[random].clone();
+
+        m
+    }
+}

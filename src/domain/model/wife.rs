f7e4384860d1142cd51d0df3ae38ce0ae2220a33
Boris <glazunov2142@icloud.com>
2025-02-22 09:31:52 +0300

    Update wife.rs

--- src/domain/model/wife.rs
+++ src/domain/model/wife.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Message {
     pub text: String,
 }

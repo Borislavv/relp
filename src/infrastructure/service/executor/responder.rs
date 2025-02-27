3204d0dcf2ba663cf361ee2305f4a347224eb28d
Boris <glazunov2142@icloud.com>
2025-02-27 10:56:25 +0300

    Update responder.rs

--- src/infrastructure/service/executor/responder.rs
+++ src/infrastructure/service/executor/responder.rs
@@ -3,6 +3,7 @@ use crate::infrastructure::integration::telegram;
 use crate::infrastructure::model::command::Exit;
 use std::error::Error;
 use std::sync::Arc;
+use crate::domain::r#enum::exit_code::ExitCode;
 
 pub trait Responder: Send + Sync {
     fn respond(&self, exit_state: Exit) -> Result<(), Box<dyn Error>>;
@@ -24,25 +25,38 @@ impl ExitCommandResponder {
 
 impl Responder for ExitCommandResponder {
     fn respond(&self, exit: Exit) -> Result<(), Box<dyn Error>> {
-        match self.telegram.send_message(
-            self.cfg.chat_id,
-            format!(
-                "```Input:\t{}```
+        if exit.code == ExitCode::Wife {
+            match self.telegram.send_message(
+                self.cfg.chat_id,
+                format!("{}", exit.stdout.as_str()).as_str()
+            ) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e),
+            }
+        } else {
+            match self.telegram.send_message(
+                self.cfg.chat_id,
+                format!(
+                    "```Input:\t{}```
                 ```Stdout:\t{}```
                 ```Stderr:\t{}```
                 ```Code:\t{}```",
-                match exit.input_message {
-                    Some(msg) => msg.text,
-                    None => "".to_string(),
-                },
-                exit.stdout.as_str(),
-                exit.stderr.as_str(),
-                exit.code,
-            )
-            .as_str(),
-        ) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(e),
+                    match exit.input_message {
+                        Some(msg) => msg.text,
+                        None => "".to_string(),
+                    },
+                    exit.stdout.as_str(),
+                    exit.stderr.as_str(),
+                    match exit.code {
+                        ExitCode::Success => 0,
+                        ExitCode::Failed => 1,
+                        _ => panic!("undefined exit code")
+                    },
+                ).as_str(),
+            ) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e),
+            }
         }
     }
 }

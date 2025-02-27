b1f9991fac8a40c3452278e49a298faabefd5628
Boris <glazunov2142@icloud.com>
2025-02-27 09:22:08 +0300

    Update wife.rs

--- src/domain/error/wife.rs
+++ src/domain/error/wife.rs
@@ -17,3 +17,20 @@ impl fmt::Display for WifeMessagesVecIsEmptyError {
 }
 
 impl Error for WifeMessagesVecIsEmptyError {}
+
+#[derive(Debug)]
+pub struct WifeMessageIsNoneError {}
+
+impl WifeMessageIsNoneError {
+    pub fn new() -> WifeMessageIsNoneError {
+        WifeMessageIsNoneError {}
+    }
+}
+
+impl fmt::Display for WifeMessageIsNoneError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Wife message is None.")
+    }
+}
+
+impl Error for WifeMessageIsNoneError {}
